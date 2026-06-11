63220a9de3e1290ad145a64fc5a04d742d65ed79
Anass <anassbellagrid@gmail.com>
2026-06-11 10:46:01 +0000

    delete some files

--- patch_requests.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-// RequestsList
-let reqList = fs.readFileSync('src/pages/RequestsList.tsx', 'utf8');
-reqList = reqList.replace(/const MOCK_REQUESTS[ \s\S]*?\];\n\n/, '');
-reqList = reqList.replace(/const displayedRequests = \(requests\.length > 0 \? requests : MOCK_REQUESTS\).filter\(req => !categoryFilter \|\| req.category === categoryFilter\);/, 
-  'const displayedRequests = requests.filter(req => !categoryFilter || req.category === categoryFilter);');
-reqList = reqList.replace(/\{requests\.length === 0 \? "Exemples de Missions" : "Missions Actuelles"\}/, '"Missions Actuelles"');
-fs.writeFileSync('src/pages/RequestsList.tsx', reqList);
